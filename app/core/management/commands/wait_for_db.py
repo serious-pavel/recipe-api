1a64beb71be6ae30315d4555bcc2cc32783bf67a
serious-pavel <pvmakhnev@gmail.com>
2025-02-06 14:10:56 +0100

    change wait_for_db, force to run real query, check passed while database wasn't ready

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -3,6 +3,7 @@ Django command to wait for the database to be available
 """
 
 import time
+from django.db import connections
 from psycopg2 import OperationalError as Psycopg2Error
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
@@ -14,14 +15,13 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         """Entry point for the management command"""
         self.stdout.write('Waiting for database...')
-        db_up = False
+        db_conn = None
 
-        while not db_up:
+        while not db_conn:
             try:
-                self.check(databases=['default'])
-                db_up = True
+                db_conn = connections['default']
+                db_conn.cursor()
             except (OperationalError, Psycopg2Error):
                 self.stdout.write('DB is unavailable. Waiting for 1 second...')
                 time.sleep(1)
-
         self.stdout.write(self.style.SUCCESS('Database available!'))
