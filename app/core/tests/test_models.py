9a272a3df62626077c07a59782dc7a8661f0cd55
serious-pavel <pvmakhnev@gmail.com>
2025-02-10 16:52:19 +0100

    add test for creating user without an email

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -32,3 +32,9 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test creating a new user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+
