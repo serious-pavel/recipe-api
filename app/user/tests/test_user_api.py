4ac8440f825cb6d2dec7410feadb7b4db34334b5
serious-pavel <pvmakhnev@gmail.com>
2025-02-17 11:44:08 +0100

    fix typos

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -80,7 +80,7 @@ class PublicUserApiTests(TestCase):
         }
         res = self.client.post(TOKEN_URL, payload)
         self.assertIn('token', res.data)
-        res.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
 
     def test_create_token_invalid_credentials(self):
         """Test creating the token for invalid credentials"""
@@ -91,7 +91,7 @@ class PublicUserApiTests(TestCase):
         }
         res = self.client.post(TOKEN_URL, payload)
         self.assertNotIn('token', res.data)
-        res.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_token_blank_password(self):
         """Test creating the token for blank password"""
@@ -101,4 +101,4 @@ class PublicUserApiTests(TestCase):
         }
         res = self.client.post(TOKEN_URL, payload)
         self.assertNotIn('token', res.data)
-        res.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
