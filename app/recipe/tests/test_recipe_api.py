97f6e6a667602fa3fe94c0e25ae80d834d97b489
serious-pavel <pvmakhnev@gmail.com>
2025-02-20 12:43:07 +0100

    add create recipe helper

---
+++ app/recipe/tests/test_recipe_api.py
@@ -0,0 +1,30 @@
+"""
+Tests for recipe api
+"""
+
+from decimal import Decimal
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+from app.core.models import Recipe
+from app.recipe.serializers import RecipeSerializer
+
+
+def create_recipe(user, **params):
+    """Create and return a sample recipe"""
+    defaults = {
+        'title': 'Sample recipe title',
+        'time_minutes': 22,
+        'price': Decimal('5.25'),
+        'description': 'Sample recipe description',
+        'link': 'http://example.com/recipe.pdf',
+    }
+    defaults.update(params)
+
+    recipe = Recipe.objects.create(user=user, **defaults)
+    return recipe
