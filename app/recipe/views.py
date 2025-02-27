6ba1dd082a33b90778330d18d9566bac1343ebce
serious-pavel <pvmakhnev@gmail.com>
2025-02-27 10:56:32 +0100

    add ingredient view

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -6,7 +6,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
 from recipe import serializers
 
 
@@ -39,6 +39,19 @@ class TagViewSet(mixins.DestroyModelMixin,
     """ViewSet for viewing and editing tags"""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class IngredientViewSet(mixins.ListModelMixin,
+                        viewsets.ModelViewSet):
+    """ViewSet for viewing and editing ingredients"""
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
