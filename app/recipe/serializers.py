079f6b3f840f83d6b5bebffe0e951befaca01986
serious-pavel <pvmakhnev@gmail.com>
2025-03-05 13:32:10 +0100

    add image as a visible field in API

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -88,7 +88,7 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail model"""
 
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ('description',)
+        fields = RecipeSerializer.Meta.fields + ('description', 'image')
 
 
 class RecipeImageSerializer(serializers.ModelSerializer):
