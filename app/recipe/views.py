576539e5ae1c6e61355a8a95eb98ad901af1271c
serious-pavel <pvmakhnev@gmail.com>
2025-03-06 12:28:59 +0100

    add documentation for filters

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -18,6 +18,22 @@ from core.models import Recipe, Tag, Ingredient
 from recipe import serializers
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                'tags',
+                OpenApiTypes.STR,
+                description='Comma separated list of tag IDs to filter'
+            ),
+            OpenApiParameter(
+                'ingredients',
+                OpenApiTypes.STR,
+                description='Comma separated list of ingredient IDs to filter'
+            )
+        ]
+    )
+)
 class RecipeViewSet(viewsets.ModelViewSet):
     """ViewSet for viewing and editing recipes"""
     serializer_class = serializers.RecipeDetailSerializer
