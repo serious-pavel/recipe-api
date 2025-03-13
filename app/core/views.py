732cd296b767c614337d8a206eac383ab6d99c7a
serious-pavel <pvmakhnev@gmail.com>
2025-03-13 12:09:33 +0100

    fix styling

--- app/core/views.py
+++ app/core/views.py
@@ -5,6 +5,7 @@ core views for app
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+
 @api_view(['GET'])
 def health_check(request):
     """Returns successful response"""
